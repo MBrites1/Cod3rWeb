6a81b14bb12ca6ef598cd71769473d6ee96adfbe
MBrites1 <90266869+MBrites1@users.noreply.github.com>
2021-09-08 12:16:16 -0300

    ArqRestJs Novo exemplo

--- ESNext/rest.js
+++ ESNext/rest.js
@@ -4,5 +4,21 @@ function total(...numeros) {
     return total
 }
 
-console.log(total (8, 7, 6, 5,))
+//console.log(total (8, 7, 6, 5,))
 
+var a = ['Matheus', 'Henrique', 'Cida']
+var b = ['Lucia', 'Layla', ...a]
+b.forEach(function (n) {
+    console.log(n)
+})
+
+console.log(b[3])
+
+var m = new Map()
+m.set(1, 'Matheus')
+m.set(30, 'Lucia')
+m.set(2, 'Cida')
+m.set('Layla', {nome: 'Layla', idade: 30, sexo: 'F'}) //Usando Json
+m.set(3, 'Henrique')
+
+console.log(m.get('Layla'))
